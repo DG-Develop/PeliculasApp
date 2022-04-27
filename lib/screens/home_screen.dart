eff318ab251e33dd81cc177740a61eb07bc17fc5
DG-Develop <dgdband@gmail.com>
2022-04-26 23:23:35 -0500

    [ADD] Agregando la fase inicial del proyecto

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Películas en cines'),
+          elevation: 0,
+          centerTitle: true,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: [CardSwiper()],
+        ));
+  }
+}
